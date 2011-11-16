e48ef6d7e835ab167ca1e10ecc31a44cff61d9bb
Chris Rode <cirode@gmail.com>
2011-11-16 20:53:01 +1100

    Just tested this with IE running on XP in a virtual machine. Works fine except for random IE crashes which are a problem with selenium. I will try with windows 7 soon. Apart from that I am going to start migrating this code to a gem

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,17 +1,27 @@
 require 'rubygems'
 require 'rspec'
 require "selenium-webdriver"
+require 'lazy'
 
 module BrowserHelper
-  def for_each_browser(browser_names=[:chrome, :firefox], &block)
+  def for_each_browser(browser_names=[:firefox, :chrome], &block)
+    previous_browser = nil
+    at_exit do
+      previous_browser.quit if previous_browser
+    end
     browser_names.each do |browser_name|
-      driver = Selenium::WebDriver.for browser_name
-      block.call(driver, browser_name)
-      at_exit do 
-        driver.quit
+      driver =  Lazy::Promise.new do 
+        previous_browser.quit if previous_browser
+        previous_browser = Selenium::WebDriver.for( *(respond_to?(method_name = "#{browser_name}_instance") ? send(method_name) :  [browser_name]))
       end
+      block.call(driver, browser_name)
     end
   end
+  
+  def ie_instance
+    [:remote, {:url => 'http://10.211.55.6:4444/wd/hub', :desired_capabilities => :ie}]
+  end
+  
 end
 
 RSpec.configure do |config|
