eb3a13a64ced6ea83ee10331c855909c523a659b
Chris Rode <cirode@gmail.com>
2011-11-16 18:36:22 +1100

    Now able to run against multiple browsers using the same tests

--- specs/search/user_can_search_in_google_spec.rb
+++ specs/search/user_can_search_in_google_spec.rb
@@ -1,36 +1,38 @@
 require File.join(File.dirname(__FILE__),'..','spec_helper')
+include BrowserHelper
 
-describe "searching" do
-  let(:driver) {Selenium::WebDriver.for :chrome}
-  context "when on the root path" do
-    before do
-      driver.get 'http://google.com'
-    end
-    
-    it "should have the google logo with a title Google" do
-      driver.find_element(:id => 'hplogo', :title => "Google").should_not be_nil
-    end
-    
-    it "should have a search box" do
-      driver.find_element(:name => 'q', :title => "Search").should_not be_nil
-    end
-    
-    context "and the user searches for puppies" do
+for_each_browser do |driver, driver_name|
+  describe "searching in #{driver_name}" do
+    context "when on the root path" do
       before do
-        driver.find_element(:name => 'q').send_keys "Puppies"
-        wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
-        @results_element = nil
-        wait.until { @results_element = driver.find_element(:id => "ires") }
-        @results_element
+        driver.get 'http://google.com'
       end
-      
-      it "should automatically show results" do
-        @results_element.find_elements(:class => 'g').should_not be_empty
+    
+      it "should have the google logo with a title Google" do
+        driver.find_element(:id => 'hplogo', :title => "Google").should_not be_nil
       end
+    
+      it "should have a search box" do
+        driver.find_element(:name => 'q', :title => "Search").should_not be_nil
+      end
+    
+      context "and the user searches for puppies" do
+        before do
+          driver.find_element(:name => 'q').send_keys "Puppies"
+          wait = Selenium::WebDriver::Wait.new(:timeout => 2) # seconds
+          @results_element = nil
+          wait.until { @results_element = driver.find_element(:id => "ires") }
+          @results_element
+        end
       
-      it "should show the Everything navigation item selected" do
-        nav = driver.find_element(:id => 'leftnav')
-        nav.find_element(:class => 'msel').text.should == "Everything"
+        it "should automatically show results" do
+          @results_element.find_elements(:class => 'g').should_not be_empty
+        end
+      
+        it "should show the Everything navigation item selected" do
+          nav = driver.find_element(:id => 'leftnav')
+          nav.find_element(:class => 'msel').text.should == "Everything"
+        end
       end
     end
   end
--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,6 +2,18 @@ require 'rubygems'
 require 'rspec'
 require "selenium-webdriver"
 
+module BrowserHelper
+  def for_each_browser(browser_names=[:chrome, :firefox], &block)
+    browser_names.each do |browser_name|
+      driver = Selenium::WebDriver.for browser_name
+      block.call(driver, browser_name)
+      at_exit do 
+        driver.quit
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.before(:suite) do
     #some sort of setup here. Make sure we can connect to teh testable version of the app etc
@@ -11,7 +23,6 @@ RSpec.configure do |config|
     #we will need to create data etc to work properly, else the suite will atke forever to run
   end
   
-  config.after(:each) do
-    driver.quit
+  config.after(:suite) do
   end
 end
